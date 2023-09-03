04fc323b3e4060bc945ced8ab6d24ad050e99761
LuttaKing <denilsonwash@gmail.com>
2023-09-03 21:38:24 +0300

    gdrgh

--- main.py
+++ main.py
@@ -1,9 +1,74 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+import crud, models, schema
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-books = ['book1','book2']
 
-@app.get("/")
-def rea():
-    return books
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/", response_model=schema.User)
+def create_user(user: schema.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=list[schema.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schema.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schema.Item)
+def create_item_for_user(
+    user_id: int, item: schema.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/", response_model=list[schema.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
+
+
+# books = ['book1','book2']
+
+# @app.get("/books")
+# def get_all_books():
+#     return {'books':books}
+
+# @app.get("/books/{book_id}")
+# async def get_book(book_id:int):
+#     return {'book_id':book_id}
+
+# @app.post("/books")
+# async def create_book(book:Book):
+#     books.append(book)
+#     return {'message':f'{book.name} has been added','book':book}
+
+
+        
+
+
